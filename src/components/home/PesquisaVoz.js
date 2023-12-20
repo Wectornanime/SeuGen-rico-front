2a6c6cc11b6a90c1340f63a88e0aa6f9be74ac18
Wectornanime <Wectornanime@gmail.com>
2023-12-20 20:43:27 -0300

    Samuel

--- src/components/home/PesquisaVoz.js
+++ src/components/home/PesquisaVoz.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import './PesquisaVoz.css';
 import Microfone from '../../assets/microfone.svg';
-import MostrarDadosVoz from './MostrarDadosVoz'; // Importe o componente MostrarDadosVoz
 
 function PesquisaVoz() {
     const [dadosGravados, setDadosGravados] = useState(null);
     const [ouvindo, setOuvindo] = useState(false);
-    let recognition = null;
 
     const iniciarGravacao = () => {
-        recognition = new window.webkitSpeechRecognition();
+        const recognition = new window.webkitSpeechRecognition();
         recognition.lang = 'pt-BR';
 
         recognition.onstart = () => {
@@ -22,11 +20,25 @@ function PesquisaVoz() {
                 textoCapturado: vozCapturada,
                 // Outros campos que você deseje adicionar
             };
-            setDadosGravados(dadosJSON);
+            setDadosGravados(dadosJSON); // Salva os dados capturados como um objeto JSON no estado
             setOuvindo(false);
+            preencherInput(vozCapturada); // Chama a função para preencher o campo de input com o texto capturado
+            realizarPesquisa(vozCapturada); // Chama a função para realizar a pesquisa com o texto capturado
         };
 
         recognition.start();
+    };
+
+    const preencherInput = (texto) => {
+        const inputPesquisa = document.getElementById('medicamento');
+        if (inputPesquisa) {
+            inputPesquisa.value = texto; // Preenche o campo de input com o texto capturado
+        }
+    };
+
+    const realizarPesquisa = (textoPesquisa) => {
+        // Adicione aqui a lógica para realizar a pesquisa com o texto capturado
+        // Por exemplo: realizar a busca ao disparar um evento de mudança (change) no input ou chamando a função que realiza a busca
     };
 
     return (
@@ -36,8 +48,6 @@ function PesquisaVoz() {
                     <span>{ouvindo ? 'Escutando' : 'Pesquisa por voz'}</span>
                     <img src={Microfone} alt="Ícone do Microfone" />
                 </button>
-                {/* Passando os dados para o componente MostrarDadosVoz */}
-                <MostrarDadosVoz dados={dadosGravados} />
             </div>
         </div>
     );
