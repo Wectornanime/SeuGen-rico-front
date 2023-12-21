9566c6f020b62f2a21430b25adc6634de55c3d74
Wectornanime <Wectornanime@gmail.com>
2023-12-20 21:40:30 -0300

    init back integration with back

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import CategoriaNome2 from './components/home/CategoriaNome2';
 import Carrossel2 from './components/home/Carrossel2';
 import CategoriaNome3 from './components/home/CategoriaNome3';
 import Carrossel3 from './components/home/Carrossel3';
+import { useState } from 'react';
 
 
 function App() {
+  const [remedysState, setRemedysState] = useState(null);
+
   return (
     <main className="principal">
         <Cabecalho />
-        <Pesquisa />
+        <Pesquisa remedysState={remedysState} setRemedysState={setRemedysState} />
         <PesquisaVoz />
-        <CategoriaNome1 />
+
+
+        {
+          remedysState ? (<CategoriaNome1 />) : (null)
+        }
+
+        {/* <CategoriaNome1 />
         <Carrossel1 />
         <CategoriaNome2 />
         <Carrossel2 />
         <CategoriaNome3 />
-        <Carrossel3 />
+        <Carrossel3 /> */}
     </main>
   );
 }
